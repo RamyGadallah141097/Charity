cfcfd3c59f262c34e76cb677b75e00067e53c5be
kariemibrahiem <kariemibrahiem@gmail.com>
2025-04-03 13:46:59 +0200

    upload

--- resources/views/admin/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container">
-        <div class="row align-items-center flex-row-reverse">
-            <div class="col-md-12 col-sm-12 text-center">
-                <a  href="https://topbuziness.com/" class="text-muted">Powered By Top Business For Solutions</a>
-            </div>
-        </div>
-    </div>
-</footer>
