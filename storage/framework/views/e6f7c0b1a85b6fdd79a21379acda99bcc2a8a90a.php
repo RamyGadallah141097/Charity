fd73f2fe8a93d1941150f035a1b08a30be44ce82
kariemibrahiem <144341428+kariemibrahiem@users.noreply.github.com>
2025-08-03 16:15:57 +0300

    Delete storage directory

--- storage/framework/views/e6f7c0b1a85b6fdd79a21379acda99bcc2a8a90a.php
+++
@@ -1,4 +0,0 @@
-<div id="global-loader">
-    <img src="<?php echo e(asset('uploads/loader2.png')); ?>" class="mt-10" style="max-height: 300px" alt="Loader">
-</div>
-<?php /**PATH /home/kariem/Desktop/Projects/new-zakat/resources/views/admin/layouts/loader.blade.php ENDPATH**/ ?>
